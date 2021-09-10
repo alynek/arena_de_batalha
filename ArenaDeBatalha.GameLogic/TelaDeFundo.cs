fe9c107b9e94ad3c223f4058feee168cec07f7a1
Alyne Gois <alyne0x67@gmail.com>
2021-09-10 17:08:39 -0300

    Adiciona classe TelaDeFundo

---
+++ ArenaDeBatalha.GameLogic/TelaDeFundo.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace ArenaDeBatalha.GameLogic
+{
+    public class TelaDeFundo : ObjetoBase
+    {
+        public TelaDeFundo(Size limites, Graphics tela) : base(limites, tela)
+        {
+            Esquerda= 0;
+            Topo = 0;
+            Velocidade = 0;
+        }
+
+        public override Bitmap ObterImagem()
+        {
+            return Media.cosmos;
+        }
+    }
+}
