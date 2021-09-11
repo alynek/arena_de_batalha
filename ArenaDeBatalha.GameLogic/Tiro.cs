5e23db5951823b9c1a0d1db4f163647d3833056f
Alyne Gois <alyne0x67@gmail.com>
2021-09-11 12:52:30 -0300

    Adiciona classe Tiro

---
+++ ArenaDeBatalha.GameLogic/Tiro.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace ArenaDeBatalha.GameLogic
+{
+    public class Tiro : ObjetoBase
+    {
+        public Tiro(Size limites, Graphics telaDePintura, Point posicao):base(limites, telaDePintura)
+        {
+            this.Velocidade = 20;
+            this.Som = Media.Missile;
+            this.Esquerda = posicao.X;
+            this.Topo = posicao.Y;
+            this.TocarSom();
+        }
+
+        public override Bitmap ObterImagem()
+        {
+            return Media.laser;
+        }
+
+        public override void AtualizarObjeto()
+        {
+            this.MoverParaCima();
+            base.AtualizarObjeto();
+        }
+    }
+}
