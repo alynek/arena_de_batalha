9701e3a5806bf2f86e92b40e2774ae953ad4f23d
Alyne Gois <alyne0x67@gmail.com>
2021-09-10 22:40:17 -0300

    Adiciona classe Inimigo

---
+++ ArenaDeBatalha.GameLogic/Inimigo.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace ArenaDeBatalha.GameLogic
+{
+    public class Inimigo : ObjetoBase
+    {
+        public Inimigo(Size limites, Graphics telaDePintura, Point posicao) : base(limites, telaDePintura)
+        {
+            this.Esquerda = posicao.X;
+            this.Topo = posicao.Y;
+            this.Velocidade = 5;
+            this.Som = Media.exploshion_short;
+        }
+
+        public override Bitmap ObterImagem()
+        {
+            return Media.inimigo;
+        }
+
+        public override void AtualizarObjeto()
+        {
+            this.MoverParaBaixo();
+            base.AtualizarObjeto();
+        }
+    }
+}
