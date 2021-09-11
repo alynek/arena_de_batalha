575ea69c520bc47436bf06feaf3661cdf285c5d3
Alyne Gois <alyne0x67@gmail.com>
2021-09-11 12:52:13 -0300

    Adiciona classe FimdeJogo

---
+++ ArenaDeBatalha.GameLogic/FimDeJogo.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace ArenaDeBatalha.GameLogic
+{
+    public class FimDeJogo : ObjetoBase
+    {
+        public FimDeJogo(Size limites, Graphics telaDePintura) : base(limites, telaDePintura)
+        {
+            this.Esquerda = (this.Limites.Width / 2) - (this.Largura / 2);
+            this.Topo = (this.Limites.Width / 2) - (this.Largura / 2);
+            this.Velocidade = 0;
+        }
+
+        public override Bitmap ObterImagem()
+        {
+            return Media.gameOver;
+        }
+    }
+}
